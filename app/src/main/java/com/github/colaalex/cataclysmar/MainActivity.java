852e314ec7f7cbeca43708d3548f0ba49fedada9
colaalex <colaalex4r@mail.ru>
2019-02-02 22:32:41 +0300

    axis angle

--- app/src/main/java/com/github/colaalex/cataclysmar/MainActivity.java
+++ app/src/main/java/com/github/colaalex/cataclysmar/MainActivity.java
@@ -2,7 +2,6 @@ package com.github.colaalex.cataclysmar;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MotionEvent;
 
 import com.google.ar.core.Anchor;
@@ -11,12 +10,13 @@ import com.google.ar.core.Plane;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.Node;
 import com.google.ar.sceneform.Scene;
+import com.google.ar.sceneform.math.Quaternion;
 import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.Color;
 import com.google.ar.sceneform.rendering.MaterialFactory;
 import com.google.ar.sceneform.rendering.Renderable;
 import com.google.ar.sceneform.rendering.ShapeFactory;
 import com.google.ar.sceneform.rendering.Texture;
-import com.google.ar.sceneform.rendering.ViewRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
 
@@ -30,6 +30,8 @@ import static java.lang.Math.toRadians;
 public class MainActivity extends AppCompatActivity {
 
     private static final float RADIUS = 0.1f;
+    private static final float LIFT = 0.15f;
+
     private float lat = 55.751244f;
     //private float lat = 0f;
     private float lon = 37.618423f;
@@ -57,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
         builder.build().thenAccept(texture ->
                 MaterialFactory.makeOpaqueWithTexture(this, texture).thenAccept(
                         material -> renderable =
-                                ShapeFactory.makeSphere(RADIUS, new Vector3(0.0f, 0.15f, 0.0f), material)
+                                ShapeFactory.makeSphere(RADIUS, new Vector3(0.0f, LIFT, 0.0f), material)
                 ));
 
         Scene scene = arFragment.getArSceneView().getScene();
@@ -89,20 +91,31 @@ public class MainActivity extends AppCompatActivity {
                         float z = (float) (RADIUS * sin(theta) * cos(phi));
 
                         Node pinNode = new Node();
-                        ViewRenderable.builder()
-                                .setView(this, R.layout.map_pin)
-                                .build()
-                                .thenAccept(viewRenderable -> {
-                                    viewRenderable.setSizer(view -> new Vector3(0.1f, 0.1f, 0.1f));
-                                    pinNode.setRenderable(viewRenderable);
-                                });
-//                    MaterialFactory.makeOpaqueWithColor(this, new Color(android.graphics.Color.RED))
-//                            .thenAccept(
-//                                    material -> pinRenderable =
-//                                            ShapeFactory.makeCube(new Vector3(0.2f, 0.2f, 0.2f), new Vector3(x, y, z), material));
-                        pinNode.setRenderable(pinRenderable);
-                        pinNode.setParent(earth);
-                        pinNode.setLocalPosition(new Vector3(x, y + 0.05f, z));
+//                        ViewRenderable.builder()
+//                                .setView(this, R.layout.map_pin)
+//                                .build()
+//                                .thenAccept(viewRenderable -> {
+//                                    viewRenderable.setSizer(view -> new Vector3(0.1f, 0.1f, 0.1f));
+//                                    pinNode.setRenderable(viewRenderable);
+//                                });
+                        MaterialFactory.makeOpaqueWithColor(this, new Color(android.graphics.Color.RED))
+                                .thenAccept(
+                                        material -> {
+                                            pinRenderable =
+                                                    ShapeFactory.makeCylinder(0.001f, 0.01f, Vector3.zero(), material);
+                                            pinNode.setRenderable(pinRenderable);
+                                            pinNode.setParent(earth);
+                                            pinNode.setLocalPosition(new Vector3(x, 0.5f * RADIUS + y, z));
+                                            //pinNode.setLocalPosition(Vector3.zero());
+                                            Quaternion q1 = Quaternion.axisAngle(Vector3.forward(), lat + 90);
+                                            Quaternion q2 = Quaternion.axisAngle(Vector3.left(), lon);
+                                            pinNode.setLocalRotation(Quaternion.multiply(q1, q2));
+                                        }
+                                );
+                        //pinNode.setRenderable(pinRenderable);
+//                        pinNode.setParent(earth);
+//                        pinNode.setLocalPosition(new Vector3(0, 0, 0));
+                        //pinNode.setLocalRotation(Quaternion.axisAngle(Vector3.up(), (float) theta));
                     }
                 }
         );
