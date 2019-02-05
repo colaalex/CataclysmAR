d473bb6803516bdf808b4b494b7b64cec3451314
Alex <colaalex4r@mail.ru>
2019-02-05 17:20:40 +0300

    australia and africa sorted

--- app/src/main/java/com/github/colaalex/cataclysmar/domain/CSVWorker.java
+++ app/src/main/java/com/github/colaalex/cataclysmar/domain/CSVWorker.java
@@ -1,5 +1,7 @@
 package com.github.colaalex.cataclysmar.domain;
 
+import android.util.Log;
+
 import com.github.colaalex.cataclysmar.domain.entity.Wildfire;
 
 import java.io.BufferedReader;
@@ -7,19 +9,22 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import static com.github.colaalex.cataclysmar.domain.entity.Coordinates.AFRICA_EAST;
+import static com.github.colaalex.cataclysmar.domain.entity.Coordinates.AFRICA_NORTH;
+import static com.github.colaalex.cataclysmar.domain.entity.Coordinates.AFRICA_SOUTH;
+import static com.github.colaalex.cataclysmar.domain.entity.Coordinates.AFRICA_WEST;
+import static com.github.colaalex.cataclysmar.domain.entity.Coordinates.AUSTRALIA_EAST;
+import static com.github.colaalex.cataclysmar.domain.entity.Coordinates.AUSTRALIA_NORTH;
+import static com.github.colaalex.cataclysmar.domain.entity.Coordinates.AUSTRALIA_SOUTH;
+import static com.github.colaalex.cataclysmar.domain.entity.Coordinates.AUSTRALIA_WEST;
 
 public class CSVWorker {
 
-    private static final float AFRICA_NORTH = 37.34f;
-    private static final float AFRICA_SOUTH = -34.82f;
-    private static final float AFRICA_WEST = -17.53f;
-    private static final float AFRICA_EAST = 51.4f;
-
-    private static final float AUSTRALIA_NORTH = -10.689167f;
-    private static final float AUSTRALIA_SOUTH = -43.644444f;
-    private static final float AUSTRALIA_WEST = 113.155f;
-    private static final float AUSTRALIA_EAST = 153.637222f;
+    private static final int AFRICA = 0;
+    private static final int AUSTRALIA = 1;
 
     private InputStream inputStream;
 
@@ -29,7 +34,10 @@ public class CSVWorker {
 
     public List<List<Float>> read() {
         //List<String[]> list = new ArrayList<>();
-        List<Wildfire> wildfires = new ArrayList<>();
+
+        List<Wildfire> africanWildfires = new ArrayList<>();
+        List<Wildfire> australianWildfires = new ArrayList<>();
+
         List<List<Float>> coordinates = new ArrayList<>();
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
@@ -46,12 +54,24 @@ public class CSVWorker {
                     float lat = Float.parseFloat(row[0]);
                     float lon = Float.parseFloat(row[1]);
 
-                    if (lat >= AUSTRALIA_SOUTH && lat <= AUSTRALIA_NORTH && lon >= AUSTRALIA_WEST && lon <= AUSTRALIA_EAST) {
-                        pair.add(lat);
-                        pair.add(lon);
-                        coordinates.add(pair);
-                        wildfires.add(new Wildfire(lat, lon, Integer.parseInt(row[8])));
+//                    if (lat >= AUSTRALIA_SOUTH && lat <= AUSTRALIA_NORTH && lon >= AUSTRALIA_WEST && lon <= AUSTRALIA_EAST) {
+//                        pair.add(lat);
+//                        pair.add(lon);
+//                        coordinates.add(pair);
+//                        wildfires.add(new Wildfire(lat, lon, Integer.parseInt(row[8])));
+//                    }
+
+                    switch (determineContinent(lat, lon)) {
+                        case AFRICA:
+                            africanWildfires.add(new Wildfire(lat, lon, Integer.parseInt(row[8].replaceAll("\\s", ""))));
+                            break;
+                        case AUSTRALIA:
+                            australianWildfires.add(new Wildfire(lat, lon, Integer.parseInt(row[8].replaceAll("\\s", ""))));
+                            break;
+                        default:
+                            break;
                     }
+
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -66,6 +86,45 @@ public class CSVWorker {
             }
         }
 
+        Collections.sort(africanWildfires, (wildfire, t1) -> {
+            if (wildfire.getConfidence() == t1.getConfidence())
+                return 0;
+            return wildfire.getConfidence() < t1.getConfidence() ? -1 : 1;
+        });
+
+        Collections.sort(australianWildfires, (wildfire, t1) -> {
+            if (wildfire.getConfidence() == t1.getConfidence())
+                return 0;
+            return wildfire.getConfidence() < t1.getConfidence() ? -1 : 1;
+        });
+
+        for (int i = 0; i < 200 && i < africanWildfires.size(); i++) {
+            List<Float> pair = new ArrayList<>();
+            pair.add(africanWildfires.get(i).getLatitude());
+            pair.add(africanWildfires.get(i).getLongitutde());
+            coordinates.add(pair);
+        }
+
+        for (int i = 0; i < 200 && i < australianWildfires.size(); i++) {
+            List<Float> pair = new ArrayList<>();
+            pair.add(australianWildfires.get(i).getLatitude());
+            pair.add(australianWildfires.get(i).getLongitutde());
+            coordinates.add(pair);
+        }
+
+        Log.d("Africa length: ", String.valueOf(africanWildfires.size()));
+        Log.d("Australia length: ", String.valueOf(australianWildfires.size()));
+        Log.d("Coordiantes length: ", String.valueOf(coordinates.size()));
+
         return coordinates;
+    }
+
+    private int determineContinent(float lat, float lon) {
+        if (lat >= AUSTRALIA_SOUTH && lat <= AUSTRALIA_NORTH && lon >= AUSTRALIA_WEST && lon <= AUSTRALIA_EAST)
+            return AUSTRALIA;
+        else if (lat >= AFRICA_SOUTH && lat <= AFRICA_NORTH && lon >= AFRICA_WEST && lon <= AFRICA_EAST)
+            return AFRICA;
+        else
+            return -1;
     }
 }
---
+++ app/src/main/java/com/github/colaalex/cataclysmar/domain/entity/Coordinates.java
@@ -0,0 +1,13 @@
+package com.github.colaalex.cataclysmar.domain.entity;
+
+public class Coordinates {
+    public static final float AFRICA_NORTH = 37.34f;
+    public static final float AFRICA_SOUTH = -34.82f;
+    public static final float AFRICA_WEST = -17.53f;
+    public static final float AFRICA_EAST = 51.4f;
+
+    public static final float AUSTRALIA_NORTH = -10.689167f;
+    public static final float AUSTRALIA_SOUTH = -43.644444f;
+    public static final float AUSTRALIA_WEST = 113.155f;
+    public static final float AUSTRALIA_EAST = 153.637222f;
+}
